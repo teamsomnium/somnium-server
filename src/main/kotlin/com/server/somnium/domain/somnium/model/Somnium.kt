77ef16d96b83032e9efe46b0597673b153543397
taem1n1 <youman7953@naver.com>
2022-09-20 17:42:25 +0900

    v1.0.0 somnium 초기 entity 모델링

--- src/main/kotlin/com/server/somnium/domain/somnium/model/Somnium.kt
+++ src/main/kotlin/com/server/somnium/domain/somnium/model/Somnium.kt
@@ -1,4 +1,29 @@
 package com.server.somnium.domain.somnium.model
 
-class somnium {
+import com.server.somnium.domain.user.model.User
+import com.server.somnium.global.model.BaseTimeEntity
+import javax.persistence.*
+
+@Entity(name = "somnium")
+class Somnium(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    val id: Long? = null,
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    val user: User,
+
+    @Column(name = "status", nullable = false)
+    @Enumerated(EnumType.STRING)
+    val status: Status,
+
+    @Column(name = "description", nullable = false)
+    val description: String,
+
+    @Column(name = "image_url", nullable = true, length = 500)
+    val imageUrl: String
+
+): BaseTimeEntity() {
+
 }
