545b9a799643fc2e1414ecb689a9cef5f239c016
taem1n1 <youman7953@naver.com>
2022-09-21 17:35:08 +0900

    test

--- src/main/kotlin/com/server/somnium/SomniumApplication.kt
+++ src/main/kotlin/com/server/somnium/SomniumApplication.kt
@@ -3,10 +3,19 @@ package com.server.somnium
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import java.util.*
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 @EnableJpaAuditing
-class SomniumApplication
+class SomniumApplication {
+	@PostConstruct
+	fun applicationTimeZoneSetter() {
+
+		val timeZone = TimeZone.getTimeZone("Asia/Seoul")
+		TimeZone.setDefault(timeZone)
+	}
+}
 
 fun main(args: Array<String>) {
 	runApplication<SomniumApplication>(*args)
