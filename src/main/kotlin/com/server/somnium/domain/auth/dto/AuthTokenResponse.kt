d1a1ca99546587299ded76ad7306e98e6fdd2a6b
taem1n1 <youman7953@naver.com>
2022-09-28 15:21:50 +0900

    kakao oauth관련 token을 제공받는 dto

--- src/main/kotlin/com/server/somnium/domain/auth/dto/AuthTokenResponse.kt
+++ src/main/kotlin/com/server/somnium/domain/auth/dto/AuthTokenResponse.kt
@@ -1,4 +1,10 @@
 package com.server.somnium.domain.auth.dto
 
-class AuthTokenResponse {
-}
+data class AuthTokenResponse (
+    val token_type: String,
+    val access_token: String,
+    val expires_in: Int,
+    val refresh_token: String,
+    val refresh_token_expires_in: Int,
+    val scope: String
+)
