25abb5179df8b9850e47532b6e9a20af34c98c12
taem1n1 <youman7953@naver.com>
2022-09-28 15:18:22 +0900

    AuthId parameter를 가지고 user를 조회하는 단건 쿼리

--- src/main/kotlin/com/server/somnium/domain/user/repository/UserRepository.kt
+++ src/main/kotlin/com/server/somnium/domain/user/repository/UserRepository.kt
@@ -1,4 +1,8 @@
 package com.server.somnium.domain.user.repository
 
-class UserRepository {
+import com.server.somnium.domain.user.model.User
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface UserRepository: JpaRepository<User, Long> {
+    fun findUserByAuthId(authId: Long): User?
 }
