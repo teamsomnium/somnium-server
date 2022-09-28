df7618029be2827fe95d1a0e3e739076e73b1b3b
taem1n1 <youman7953@naver.com>
2022-09-28 15:21:05 +0900

    ignore된 application.yml의 oauth 정보를 property binding하는 class

--- src/main/kotlin/com/server/somnium/domain/auth/property/AuthProperties.kt
+++ src/main/kotlin/com/server/somnium/domain/auth/property/AuthProperties.kt
@@ -1,4 +1,16 @@
 package com.server.somnium.domain.auth.property
 
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+
+@Configuration
 class AuthProperties {
+
+    @Value("\${auth.client-id}")
+    lateinit var clientId: String
+    @Value("\${auth.redirect-uri}")
+    lateinit var redirectUri: String
+    @Value("\${auth.client-secret}")
+    lateinit var clientSecret: String
 }
+
